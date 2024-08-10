14079c2097404c3754aa2b9a586e424ab846ef9f
athulakumara604 <143864889+athulakumara604@users.noreply.github.com>
2024-08-10 23:16:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG : process.env.ALIVE_IMG || "",
+ALIVE_MSG : process.env.ALIVE_MSG || "Hello i am YourName i am alive now ❤️‍🩹@&",    
 };
