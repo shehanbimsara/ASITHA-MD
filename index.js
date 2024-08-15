442bd655e302b3c8f8f4955f5f35596763810e0e
athulakumara604 <143864889+athulakumara604@users.noreply.github.com>
2024-08-16 05:26:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94779415698']
+const ownerNumber = ['94789123880']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
